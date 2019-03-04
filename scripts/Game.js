7bb0075f3c1e201b498e96cc866bbfa5cd7f1b1a
MegaMeyta <47866855+MegaMeyta@users.noreply.github.com>
2019-03-03 17:58:18 -0700

    Update Game.js

--- scripts/Game.js
+++ scripts/Game.js
@@ -25,7 +25,7 @@ function preload() {
   game.load.spritesheet('trigger', 'images/jf2small.png', 50, 50, 1);
   game.load.spritesheet('msgBox', 'images/Light-Blue-Box.jpg', 450, 150, 1);
   game.load.spritesheet('speaker', 'images/Square.jpg', 80, 100, 1);
-  game.load.spritesheet('coin', 'images/coin.jpg', 50,50);
+  game.load.spritesheet('coin', 'images/coin.png', 50,50);
   game.load.audio('music', music);
 
   game.load.bitmapFont('gem', 'gem.png', 'images/gem.xml');
