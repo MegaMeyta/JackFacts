21cd4a8c120a1d28d17f4ad565b7aaf70301e1c7
MegaMeyta <47866855+MegaMeyta@users.noreply.github.com>
2019-04-15 11:10:20 -0600

    Update Game.js

--- scripts/Game.js
+++ scripts/Game.js
@@ -19,7 +19,7 @@ function preload() {
   game.load.image('ground4' , 'images/400x100.png')
   game.load.image('Square', 'images/Circle.jpg');
   game.load.image('City', 'images/Game Background.png');
-  game.load.image('door', 'images/Door4Josh.jpg');
+  game.load.image('door', 'images/Door4Josh.png');
   game.load.spritesheet('player', 'images/avatar still 2.png',20,50);
   game.load.spritesheet('cowboy', 'images/cowboyWalk.png', 30, 38);
   game.load.spritesheet('trigger', 'images/jf2small.png', 50, 50, 1);
